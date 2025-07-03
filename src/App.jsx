cc73accbd8f1b69fb80fdb5178a09b9683c991b9
angelycontrerasr <angelycontrerasr@gmail.com>
2025-07-03 17:19:34 -0400

    add education form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import WorkInput from "./components/WorkInput.jsx";
 import OptionsMenu from "./components/optionMenu.jsx";
+import {EducationInput} from "./components/EducationInput.jsx";
 
 
 export default function App() {
@@ -17,13 +18,24 @@ export default function App() {
             desc: ''
         }
     ]);
-
+    const [userEducation, setUserEducation] = useState([
+        {
+            id:1,
+            schoolName: '',
+            degree: '',
+            startDate: '',
+            endDate: '',
+            location: '',
+            edulevel: '',
+        }
+    ])
     const [selectedId, setSelectedId] = useState(1); // Puedes conectar esto a tu OptionsMenu si quieres después
 
     return (
         <div>
             <OptionsMenu />
             <WorkInput userWork={userWork} setUserWork={setUserWork} id={selectedId} />
+            <EducationInput userEducation={userEducation} setUserEducation={setUserEducation} id={selectedId} />
         </div>
     );
 }
